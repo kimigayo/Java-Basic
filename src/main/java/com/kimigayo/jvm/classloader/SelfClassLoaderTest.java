8a0a530089844f99cdb2d9b281e8b2f7b183791c
chenbinbin <1056477503@qq.com>
2018-03-18 23:25:05 +0800

    OSGI注释

--- src/main/java/com/kimigayo/jvm/classloader/SelfClassLoaderTest.java
+++ src/main/java/com/kimigayo/jvm/classloader/SelfClassLoaderTest.java
@@ -8,7 +8,7 @@ public class SelfClassLoaderTest {
         Class<?> aClass1 = classLoader1.loadClass("com.kimigayo.jvm.classloader.Person");
         Class<?> aClass2 = classLoader2.loadClass("com.kimigayo.jvm.classloader.Person");
         System.out.println(aClass1.getClassLoader());
-        System.out.println(aClass1.hashCode());//不同类加载器加载同一个class文件，产生的class对象不同
+        System.out.println(aClass1.hashCode());
         System.out.println(aClass2.hashCode());
     }
 }
